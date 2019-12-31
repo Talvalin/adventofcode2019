e88daae94b3c4b3d152de60105a3ba0d7d49d35b
Lalith Vipulananthan <lalithv@gmail.com>
2019-12-31 15:23:10 +0900

    Added day7 part 1 tests

--- intcode_computer_tests.py
+++ intcode_computer_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import itertools
 import intcode_computer as IntcodeComputer
 
 
@@ -74,6 +75,47 @@ class IntCode_Computer_Tests(unittest.TestCase):
         self.intcode_computer.intcode = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
         self.intcode_computer.input = [9]
         self.assertEqual(self.intcode_computer.execute(), 1001)
+
+    def test_max_thruster_fixed_phase_settings(self):
+        intcode = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+        phase_settings = [4,3,2,1,0]
+        input_output_value = 0
+        
+        for setting in phase_settings:
+            # initialise amplifier with phase setting
+            inputs = [setting, input_output_value]
+            input_output_value = IntcodeComputer.IntcodeComputer(intcode, inputs).execute()
+        self.assertEqual(input_output_value, 43210)
+
+    def test_max_thruster_fixed_phase_settings2(self):
+        intcode = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
+        phase_settings = [0,1,2,3,4]
+        input_output_value = 0
+        
+        for setting in phase_settings:
+            # initialise amplifier with phase setting
+            inputs = [setting, input_output_value]
+            input_output_value = IntcodeComputer.IntcodeComputer(intcode, inputs).execute()
+        self.assertEqual(input_output_value, 54321)
+
+    def test_max_thruster_permutations(self):
+        intcode = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+        phase_setting_permutations = itertools.permutations(range(5))
+        max_thruster_signal = 0
+        max_phase_settings = []
+        
+        for phase_settings in phase_setting_permutations:
+            input_output_value = 0
+            for setting in phase_settings:
+                # initialise amplifier with phase setting
+                inputs = [setting, input_output_value]
+                input_output_value = IntcodeComputer.IntcodeComputer(intcode, inputs).execute()
+            if input_output_value > max_thruster_signal:
+                max_thruster_signal = input_output_value
+                max_phase_settings = phase_settings
+
+        self.assertEqual(max_thruster_signal, 65210)
+        self.assertEqual(max_phase_settings, (1,0,4,3,2))
 
 
 if __name__ == "__main__":
