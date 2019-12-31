5369cc71bbfbb5727f5607a4e53cd297e2a73a79
Lalith Vipulananthan <lalithv@gmail.com>
2019-12-31 14:55:42 +0900

    Modified day05 code for list-based input to Intcode computer

--- day05.py
+++ day05.py
@@ -7,7 +7,7 @@ def load_intcode():
 def main():
     # initialise Intcode computer
     intcode = load_intcode()
-    part1_output = IntcodeComputer.IntcodeComputer(intcode, [1]]).execute()
+    part1_output = IntcodeComputer.IntcodeComputer(intcode, [1]).execute()
     part2_output = IntcodeComputer.IntcodeComputer(intcode, [5]).execute()
     
     print("Part 1 output: ", part1_output)
